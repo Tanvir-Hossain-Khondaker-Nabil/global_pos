cf631f66f63127f999da5d1fca260e8fb48a8565
tanvirhossainkhondaker@gmail.com <Tanvir>
2025-11-09 18:15:53 +0600

    d

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,22 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @routes
-    @inertiaHead
-
-@if(app()->environment('local'))
     @viteReactRefresh
     @vite('resources/js/app.jsx')
-@else
-    <link rel="stylesheet" href="{{ asset('build/assets/app-1jVSF02g.css') }}">
-    <script type="module" src="{{ asset('build/assets/app-Dlfr3Kbh.js') }}"></script>
-@endif
-
+    @inertiaHead
 </head>
 
 <body>
     @inertia
 </body>
+
 </html>
